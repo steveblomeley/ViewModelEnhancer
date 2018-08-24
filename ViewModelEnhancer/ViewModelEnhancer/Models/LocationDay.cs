30bca1fd88ec165d37d4fee320fb399ccb3631a6
TAMARYN-W10\steve <steve@blomeley-it.co.uk>
2018-08-24 17:02:15 +0000

    Add "problematic" model with nested collection

---
+++ ViewModelEnhancer/ViewModelEnhancer/Models/LocationDay.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ViewModelEnhancer.Models
+{
+    public interface IHourlyForecast
+    {
+        TimeSpan Hour { get; set; }
+        string Weather { get; set; }
+    }
+
+    public interface ILocationDay<T> where T : IHourlyForecast
+    {
+        int LocationId { get; set; }
+        DateTime Date { get; set; }
+        IEnumerable<T> HourlyForecasts { get; set; }
+    }
+
+    public class HourlyForecast : IHourlyForecast
+    {
+        public TimeSpan Hour { get; set; }
+        public string Weather { get; set; }
+    }
+
+    public class LocationDay : ILocationDay<HourlyForecast>
+    {
+        public int LocationId { get; set; }
+        public DateTime Date { get; set; }
+        public IEnumerable<HourlyForecast> HourlyForecasts { get; set; }
+    }
+}

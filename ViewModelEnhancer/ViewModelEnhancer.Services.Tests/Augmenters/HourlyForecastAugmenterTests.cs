bf18b8a02b7f79ed7f8edb39aa3f9d2e99ea4236
steve <steveblomeley@yahoo.co.uk>
2018-08-27 07:52:54 +0000

    Add a unit test that ALWAYS PASSES - even when running the code fails.

---
+++ ViewModelEnhancer/ViewModelEnhancer.Services.Tests/Augmenters/HourlyForecastAugmenterTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using ViewModelEnhancer.Models;
+using ViewModelEnhancer.Services.Augmenters;
+
+namespace ViewModelEnhancer.Services.Tests.Augmenters
+{
+    public class HourlyForecastAugmenterTests
+    {
+        [Test]
+        public void AugmentMethod_AddsForecast_ToEachHour()
+        {
+            var model = new LocationDay
+            {
+                HourlyForecasts = new []
+                {
+                    new HourlyForecast{ Hour = new TimeSpan(12, 0, 0), Weather = string.Empty}
+                }
+            };
+
+            var sut = new HourlyForecastAugmenter<HourlyForecast>();
+
+            Assert.That(model.HourlyForecasts.ToArray()[0].Weather, Is.EqualTo(string.Empty));
+
+            sut.TryAugment(model);
+
+            Assert.That(model.HourlyForecasts.ToArray()[0].Weather, Is.Not.EqualTo(string.Empty));
+        }
+    }
+}
